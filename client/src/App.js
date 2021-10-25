897448739f1cfea02b80be086c4b8a824160bdf5
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-10-25 14:50:25 +0200

    CarteFrance + navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react' 
+import carteFrance from './img/carte-france.jpg'
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-     Le Bon Plan
+     <h1>Le Bon Plan</h1>
+     <NavBar/>
+     <img src={carteFrance}></img>
     </div>
   );
 }
