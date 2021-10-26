57a121bde37d5c6dc85daaa24919acca59784184
theesan-amg <theesan270@outlook.com>
2021-10-26 13:44:08 +0200

    Setting of error

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -13,5 +13,9 @@ router.post("/login", authController.signIn);
 // Creation de route pour se deconnecter  VOIR DOSSIER contollers/auth.controllers.js
 router.get("/logout", authController.logout);
 
+// 
+router.get("/:id", authController.getUserPage)
+
+
 
 module.exports = router;
