85a82e649b5656d950fecbbdf508b43876507bf9
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-10-27 17:12:37 +0200

    app.js et css

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 // import carteFrance from '../img/carte-france.jpg'
-<<<<<<< HEAD
-import Map from '../img/france.svg'
-import '../App.css'
-=======
 import Map from '../components/Map'
->>>>>>> 0cee70742c564fbe404892e11f12b9f4940fe4ed
 
 export default function Home() {
     return (
