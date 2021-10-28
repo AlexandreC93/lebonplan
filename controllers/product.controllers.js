8790f7ba894f93f992ee15b6326be6ebaa255f91
theesan-amg <theesan270@outlook.com>
2021-10-28 17:27:59 +0200

    Setting of admin in productControllers  and routes

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -17,29 +17,77 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
-module.exports.id = async (req, res, next) => {
-    try {
-        const product = await productModel.findById(req.params.id).lean()
-        res.render("product" , {
-            
+module.exports.getProduct = async (req, res, next) => {
+
+    const product = await productModel.find()
+        .then(products => {
+            res.status(200).json({
+                message: "Produit crée",
+                product: product
+            })
         })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+}
+
+
+module.exports.admin = async (req, res, next) => {
+    const { title, price, description, image, cities } = req.body
+    try {
+        console.log("BODY >>>>>>>", req.body)
+        const product = await productModel.create([{ title, price, description, image, cities }])
+        res.status(202).json({ product })
     }
+
     catch (err) {
-        console.log("erreur")
-        res.status(500).send({ err })
+        res.status(500).json({ err })
     }
 }
 
-module.exports.admin = async (req, res, next) =>{
-    const {title, price, description, image, cities} = req.body
-    try{
-        console.log("BODY >>>>>>>", req.body )
-        const product = await productModel.create([{title, price, description, image, cities}])
-        res.status(202).json({product})
-    }
+// Ajout du produit 
+module.exports.updateProduct = async (req, res) => {
+    productModel.findOne({
+        _id: req.params.id
+    }, {
+        ...req.body,
+        _id: req.params.id
+    })
+        .then(product => {
+            res.status(200).json({
+                message: "Produit Ajouter",
+                product: product
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Erreur lors de l'ajout du produit",
+                error: err
+            })
+        })
 
-    catch (err){
-        // console.error(err)
-        res.status(500).json({err})
-    }
+}
+
+// Supprimer un produit 
+module.exports.deleteProduct = async (req, res) => {
+    productModel.deleteOne({
+        _id: req.params.id
+    })
+        .then(product => {
+            res.status(200).json({
+                message: "Produit supprimer ",
+                product: product
+            })
+
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({
+                message: "Erreur lors de la suppression du produit",
+                error: err
+            })
+        })
+
 }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const productController = require('../controllers/product.controllers.js');
 
 
-router.get('/:id' , productController.id)
 // router.get('/cities/city', productController.city)
 router.post('/admin', productController.admin)
-
+router.get("/:id" , productController.getProduct)
+router.put("/:id", adminControllers.updateProduct)
 
 
 module.exports = router;
